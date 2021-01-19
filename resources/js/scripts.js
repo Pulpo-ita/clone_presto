6abdf6bee2ed12ad414d22ef9a8d0451fa772b15
Pulpo-ita <75783797+Pulpo-ita@users.noreply.github.com>
2021-01-19 17:06:39 +0100

    Initial commit

---
+++ resources/js/scripts.js
@@ -0,0 +1,15 @@
+// let navbar = document.querySelector('#navbar')
+
+// document.addEventListener('scroll',()=> {
+   
+// if (window.pageYOffset > 150) {
+//   navbar.classList.add('bg-third');
+  
+// } else {
+//   navbar.classList.remove('bg-third');
+    
+
+// }
+
+// })
+
